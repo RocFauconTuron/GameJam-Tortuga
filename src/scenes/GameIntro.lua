b229bff63dea7235a2c1ccd1d91ce5d9c8b2ab9a
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2021-11-05 14:02:01 +0100

    Añadida GameOver y fix de GmaeIntro

--- src/scenes/GameIntro.lua
+++ src/scenes/GameIntro.lua
@@ -1,36 +1,38 @@
 
 -- Engine
 local Scene = Scene or require "src/engine/Scene"
-local Timer = Timer or require "src/engine/Timer"
+local UIText = UIText or require "src/engine/UIText"
+local Entity = Entity or require "src/engine/Entity"
 
 -- Objects
 
 -- Locals
-
 local w, h = love.graphics.getDimensions()
 
 -- Class
 local GameIntro = Scene:extend()
----------------------------------
+--------------------------------
 
 function GameIntro:new()
   GameIntro.super.new(self)
-  --------------------------
+  -------------------------
+  self:addEntity(Entity(w / 2, h / 2, "assets/textures/background.png"))
+  self:addEntity(UIText(w / 2, h / 2, "GAME INTRO", "center", 72))
 end
 
 function GameIntro:update(dt)
   GameIntro.super.update(self, dt)
-  --------------------------------  
+  --------------------------------
 end
 
 function GameIntro:draw()
   GameIntro.super.draw(self)
-  ---------------------------
+  --------------------------
 end
 
 function GameIntro:reload()
   GameIntro.super.reload(self)
-  -----------------------------
+  ----------------------------
 end
 
 function GameIntro:keyPressed(key)
---
+++ src/scenes/GameOver.lua
@@ -0,0 +1,44 @@
+
+-- Engine
+local Scene = Scene or require "src/engine/Scene"
+local UIText = UIText or require "src/engine/UIText"
+local Entity = Entity or require "src/engine/Entity"
+
+-- Objects
+
+-- Locals
+local w, h = love.graphics.getDimensions()
+
+-- Class
+local GameOver = Scene:extend()
+-------------------------------
+
+function GameOver:new()
+  GameOver.super.new(self)
+  ------------------------
+  self:addEntity(Entity(w / 2, h / 2, "assets/textures/background.png"))
+  self:addEntity(UIText(w / 2, h / 2, "GAME OVER", "center", 72))
+end
+
+function GameOver:update(dt)
+  GameOver.super.update(self, dt)
+  -------------------------------
+end
+
+function GameOver:draw()
+  GameOver.super.draw(self)
+  -------------------------
+end
+
+function GameOver:reload()
+  GameOver.super.reload(self)
+  ---------------------------
+end
+
+function GameOver:keyPressed(key)
+  GameOver.super.keyPressed(self, key)
+  ------------------------------------
+  if (key == "space") then self:nextScene() end
+end
+
+return GameOver
