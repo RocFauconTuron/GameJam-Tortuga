63c2ea08618fcfc258ceae75cbb9c267eefbd177
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2021-11-08 00:26:10 +0100

    Algunos ajuts del engine

--- src/engine/Actor.lua
+++ src/engine/Actor.lua
@@ -16,7 +16,6 @@ local Actor = Entity:extend()
 function Actor:new(x, y, texture, speed, rotation)
   Actor.super.new(self, x, y, texture, rotation)
   ----------------------------------------------
-  self.forward = Vector.new(0, 0)
   self.speed = speed or 0
   self.base_speed = self.speed
 end
@@ -24,14 +23,12 @@ end
 function Actor:update(dt)
   Actor.super.update(self, dt)
   ----------------------------
-  -- self.position = self.position + self.forward * self.speed * dt
 end
 
 function Actor:reload()
   Actor.super.reload(self)
   ------------------------
   self.alive = true
-  self.forward = Vector.new(0, 0)
   self.speed = self.base_speed
 end
 
