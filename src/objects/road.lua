68282d7bd6319362ef5bc7fd8987ec873470c990
Ricard Ruiz <rruizc@edu.tecnocampus.cat>
2021-11-08 11:44:11 +0100

    Extraccion de "Segment" de la clase road e implementada en Segment.lua

--- src/objects/road.lua
+++ src/objects/road.lua
@@ -15,157 +15,83 @@ local Road = Object:extend()
 ----------------------------
 
 function Road:new()
-  self.segments = {}
-  self.segmentLength = DATA.road.segmentLenght
-  self.roadWidth = 1200
-  self.rumble_segments = 5
-  
-  self.roadLanes = 3
-  
+
+  -- Creamos la carretera
   self:createRoad()
   
-  for i=1, self.rumble_segments, 1 do
-    self.segments[i].color.road = {r = 1, g = 1, b = 1, a = 1}
-    self.segments[#self.segments - 1 - i].color.road = {r = 1, g = 1, b = 1, a = 1}
-  end
+  -- Calculamos el tamaño total de la carretera
+  self.lenght = #self.segments * DATA.segment.lenght
   
-  self.total_segments = #self.segments
-  self.roadLength = self.total_segments * self.segmentLength
-  
-end
-
-function Road:update(dt)
 end
 
 function Road:draw()
   
-  local clipBottomLine = h
-  
-  local baseSegment = self:getSegment(Camera.z)
-  local baseIndex = baseSegment.index
-  
-  local basePercent = (Camera.z % self.segmentLength) / self.segmentLength
   local x = 0
-  local dx = -(baseSegment.curve * basePercent)
+  local dx = -(self:getSegment(Camera.z).curve * ((Camera.z % DATA.segment.lenght) / DATA.segment.lenght))
   
-  for i=1, Camera.visible_segments, 1 do
+  local bottomClip = h
+  
+  -- Render de los segmentos
+  for i=2, Camera.visible_segments, 1 do
     
-    local currIndex = (baseIndex + i) % self.total_segments
-    local currSegment = self.segments[currIndex]
+    -- Recuperamos segmento a pintar
+    local index = (self:getSegment(Camera.z).index + i) % #self.segments
     
-    self:project3D(currSegment.point, x, dx)
+    -- Lo proyectamos en el mundo
+    self.segments[index]:project(x, dx)
     
+    -- ajustamos el offset de la curva
     x = x + dx
-    dx = dx + currSegment.curve
+    dx = dx + self.segments[index].curve
     
-    local currBottomLine = currSegment.point.screen.y
+    -- Actualizamos el limite de renderizado
+    local bottomLine = self.segments[index].point.screen.y
     
-    if ((i > 1) and (currBottomLine < clipBottomLine)) then
-      
-      local prevIndex = 0
-      if (currIndex > 0) then prevIndex = currIndex - 1 else prevIndex = self.total_segments - 1 end
-      local prevSegment = self.segments[prevIndex]
-      
-      local p1 = prevSegment.point.screen
-      local p2 = currSegment.point.screen
-      
-      self:drawSegment(p1.x, p1.y, p1.w, p2.x, p2.y, p2.w, currSegment.color)
-      
-      clipBottomLine = currBottomLine
-      
-      
+    -- Asguraemos no pintar por debajo de uno antes de revasarlo y que 
+    if (bottomLine < bottomClip) then
+      self.segments[index]:drawSegment(self.segments[math.min(index - 1, #self.segments - 1)])
+      bottomClip = bottomLine
     end
     
-    for i=Camera.visible_segments, 1, -1 do
-      
-      local ci = (baseIndex + i) % self.total_segments
-      local cs = self.segments[ci]
-      
-      if (cs.props) then
-        for _,v in ipairs(cs.props) do
-          local s = 1 - (i/ Camera.visible_segments) --Camera.z / cs.point.world.z
-          local sc = cs.point.scale
-          local xx = cs.point.screen.x + (sc * v[2] * self.roadWidth * w / 2)
-          local yy = cs.point.screen.y - v[1]:getHeight() * s
-          local qy = v[1]:getHeight()
-          local qd = love.graphics.newQuad(0, 0, v[1]:getWidth(), qy, v[1]:getWidth(), v[1]:getHeight())
-          love.graphics.setColor(1, 1, 1, 1)
-          love.graphics.draw(v[1], qd, xx, yy, 0, s, s)
-        end
-        
+  end
+
+  -- Render de las decoraciónes
+  -- TODO
+  for i=Camera.visible_segments, 1, -1 do
+    
+    local index = (self:getSegment(Camera.z).index + i) % #self.segments
+    
+    if (self.segments[index].props) then
+      for _,v in ipairs(self.segments[index].props) do
+        local s = 1 - (i/ Camera.visible_segments) --Camera.z / cs.point.world.z
+        local sc = self.segments[index].point.scale
+        local xx = self.segments[index].point.screen.x + (sc * v[2] * DATA.road.width * w / 2)
+        local yy = self.segments[index].point.screen.y - v[1]:getHeight() * s
+        local qy = v[1]:getHeight()
+        local qd = love.graphics.newQuad(0, 0, v[1]:getWidth(), qy, v[1]:getWidth(), v[1]:getHeight())
+        love.graphics.setColor(1, 1, 1, 1)
+        love.graphics.draw(v[1], qd, xx, yy, 0, s, s)
       end
       
     end
     
   end
-  
+
 end
 
-function Road:drawSegment(x1, y1, w1, x2, y2, w2, color)
-  
-  love.graphics.setColor(color.grass.r, color.grass.g, color.grass.b, color.grass.a)
-  love.graphics.rectangle("fill", 0, y2, w, y1 - y2)
-  
-  self:drawPolygon({x1 - w1, y1, x1 + w1, y1, x2 + w2, y2, x2 - w2, y2}, color.road)
-  
-  local rumble_w1 = w1 / 5
-  local rumble_w2 = w2 / 5
-  
-  self:drawPolygon({x1 - w1 - rumble_w1, y1, x1 - w1, y1, x2 - w2, y2, x2 - w2 - rumble_w2, y2}, color.rumble)
-  self:drawPolygon({x1 + w1 + rumble_w1, y1, x1 + w1, y1, x2 + w2, y2, x2 + w2 + rumble_w2, y2}, color.rumble)
-  
-  if (color.lane) then
-    local line_w1 = (w1 / 20) / 2
-    local line_w2 = (w2 / 20) / 2
-    
-    local lane_w1 = (w1 * 2) / self.roadLanes
-    local lane_w2 = (w2 * 2) / self.roadLanes
-    
-    local lane_x1 = x1 - w1
-    local lane_x2 = x2 - w2
-    
-    for i=2, self.roadLanes, 1 do
-      lane_x1 = lane_x1 + lane_w1
-      lane_x2 = lane_x2 + lane_w2
-      
-      self:drawPolygon({lane_x1 - line_w1, y1, lane_x1 + line_w1, y1, lane_x2 + line_w2, y2, lane_x2 - line_w2, y2}, color.lane)
-      
-    end
-    
-  end
-  
-end
-
-function Road:drawPolygon(vertexs, color)
-  love.graphics.setColor(color.r, color.g, color.b, color.a)
-  love.graphics.polygon("fill", vertexs)
-end
-
-function Road:project3D(point, x, dx)
-  -- trasladamos 
-  local transX = point.world.x - Camera.x - x - dx
-  local transY = point.world.y - Camera.y
-  local transZ = point.world.z - Camera.z
-  
-  -- escalamos
-  point.scale = Camera.distToPlane / transZ
-  
-  -- proyetctamos
-  local projectedX = point.scale * transX
-  local projectedY = point.scale * transY
-  local projectedW = point.scale * self.roadWidth
-  
-  -- escalamos a cordenada sde mundo
-  point.screen.x = math.floor((1 + projectedX) * (w / 2))
-  point.screen.y = math.floor((1 - projectedY) * (h / 2))
-  point.screen.w = math.floor(projectedW * (w / 2))
-  
-end
-
+-----------------------------------
+-- /// CONSTRUCCIÓN DE LA CARRETERA
+-----------------------------------
 function Road:createRoad()
   
-  for i=0, 10, 1 do
-    self:createSection(380, math.random(-200, 200) / 100, math.random(-2500, 2500) / 100)
+  self.segments = {}
+  
+  -- Creación del primer segmento
+  self:addSegment()
+  
+  -- Diseño de la carretera
+  for i=0, 100, 1 do
+    self:createSection(38, math.random(-200, 200) / 100, math.random(-2500, 2500) / 100)
   end
   
   for i=50, #self.segments, 10 do
@@ -175,12 +101,34 @@ function Road:createRoad()
     self:addProps(i, i, "assets/textures/roca.png", 3)
   end
     
-  self.segments[100].color.road = {r = 1, g = 1, b = 1, a = 1}
+  self.segments[1].color.road = {r = 1, g = 1, b = 1, a = 1}
   
   --self:createSection(3797)
 
 end
 
+----------------------------------------------
+-- /// GESTIÓN DE LAS SECIONES Y LOS SEGMENTOS 
+----------------------------------------------
+function Road:createSection(segments, curve, y)
+  for i=1, segments, 1 do
+    self:addSegment(curve or 0, (self.segments[#self.segments]:altitude() + (y or 0))) 
+  end
+end
+
+function Road:addSegment(curve, y) 
+  table.insert(self.segments, Segment(#self.segments, #self.segments * DATA.segment.lenght, curve or 0, y or 0))
+  self.segments[#self.segments]:setColors(DATA.cB37700, DATA.c004D0D, DATA.c804000, DATA.cCC8800)
+end
+
+function Road:getSegment(pos)
+  if (pos < 0) then pos = pos + self.lenght end
+  return self.segments[(math.floor(pos / DATA.segment.lenght) % #self.segments) + 1]
+end
+
+-------------------------------
+-- /// GESTIÓN DE LOS DECORADOS
+-------------------------------
 function Road:addProps(firstSegment, lastSegment, prop, offset)
   if ((firstSegment >= 1) and (lastSegment <= #self.segments)) then
     for i=firstSegment, lastSegment, 1 do
@@ -193,29 +141,6 @@ function Road:addProp(segment, prop, offset)
   local t = {love.graphics.newImage(prop), offset}
   if (segment.props == nil) then segment.props = {} end
   table.insert(segment.props, t) 
-end
-
-function Road:createSection(nSegments, curve, y)
-  for i=0, nSegments, 1 do
-    local yy = 0
-    if (#self.segments > 0) then yy = (self.segments[#self.segments].point.world.y + (y or 0)) end
-    self:createSegment(curve or 0, yy or 0)
-  end
-end
-
-function Road:createSegment(curve, y) 
-  table.insert(self.segments, Segment(#self.segments, #self.segments * self.segmentLength, curve, y))
-  if (#self.segments % 50 ~= 0) then
-    self.segments[#self.segments]:setColors(DATA.cB37700, DATA.c004D0D, DATA.c804000, DATA.cCC8800)
-  else
-    self.segments[#self.segments]:setColors()
-  end
-  
-end
-
-function Road:getSegment(pos)
-  if (pos < 0) then pos = pos + self.roadLength end
-  return self.segments[(math.floor(pos / self.segmentLength) % self.total_segments) + 1]
 end
 
 return Road
--- src/objects/segment.lua
+++ src/objects/segment.lua
@@ -2,6 +2,11 @@
 -- Libs
 local Object = Object or require "lib/classic"
 
+-- 
+DATA = DATA or require "src/DATA"
+Camera = Camera or require "src/objects/camera"
+local w, h = love.graphics.getDimensions()
+
 -- Class
 local Segment = Object:extend()
 ------------------------
@@ -10,20 +15,83 @@ function Segment:new(index, z, curve, y)
   
   self.index = index or 0
   self.point = {}
-  self.point.world = {x = 0, y = y, z = z}
+  self.point.world = {x = 0, y = y or 0, z = z or 0}
   self.point.screen = {x = 0, y = 0, w = 0}
   self.point.scale = -1 
   self.curve = curve or 0
+  self.lanes = DATA.segment.lanes
+  self:setColors()
+end
+
+function Segment:drawSegment(prevSegment)
   
+  local x1 = prevSegment.point.screen.x
+  local y1 = prevSegment.point.screen.y
+  local w1 = prevSegment.point.screen.w
+  
+  local x2 = self.point.screen.x
+  local y2 = self.point.screen.y
+  local w2 = self.point.screen.w
+
+  love.graphics.setColor(self.color.grass.r, self.color.grass.g, self.color.grass.b, self.color.grass.a)
+  love.graphics.rectangle("fill", 0, y2, w, y1 - y2)
+  
+  self:drawPolygon({x1 - w1, y1, x1 + w1, y1, x2 + w2, y2, x2 - w2, y2}, self.color.road)
+  
+  local rumble_w1 = w1 / 5
+  local rumble_w2 = w2 / 5
+  
+  self:drawPolygon({x1 - w1 - rumble_w1, y1, x1 - w1, y1, x2 - w2, y2, x2 - w2 - rumble_w2, y2}, self.color.rumble)
+  self:drawPolygon({x1 + w1 + rumble_w1, y1, x1 + w1, y1, x2 + w2, y2, x2 + w2 + rumble_w2, y2}, self.color.rumble)
+  
+  if (self.color.lane) then
+    local line_w1 = (w1 / 20) / 2
+    local line_w2 = (w2 / 20) / 2
+    
+    local lane_w1 = (w1 * 2) / self.lanes
+    local lane_w2 = (w2 * 2) / self.lanes
+    
+    local lane_x1 = x1 - w1
+    local lane_x2 = x2 - w2
+    
+    for i=2, self.lanes, 1 do
+      lane_x1 = lane_x1 + lane_w1
+      lane_x2 = lane_x2 + lane_w2
+      
+      self:drawPolygon({lane_x1 - line_w1, y1, lane_x1 + line_w1, y1, lane_x2 + line_w2, y2, lane_x2 - line_w2, y2}, self.color.lane)
+      
+    end
+    
+  end
 end
 
-function Segment:draw()
+function Segment:drawPolygon(vertexs, color)
+  love.graphics.setColor(color.r, color.g, color.b, color.a)
+  love.graphics.polygon("fill", vertexs)
 end
 
-function Segment:project()
+function Segment:project(x, dx)
+  -- Transladamos
+  local tx = self.point.world.x - Camera.x - x - dx
+  local ty = self.point.world.y - Camera.y
+  local tz = self.point.world.z - Camera.z
+  
+  -- Escalamos 
+  self.point.scale = Camera.distToPlane / tz
+  
+  -- Proyectamos
+  local px = self.point.scale * tx
+  local py = self.point.scale * ty
+  local pw = self.point.scale * DATA.road.width
+  
+  -- Ajustamos a las cordenadas de la pantalla
+  self.point.screen.x = math.floor((1 + px) * (w / 2))
+  self.point.screen.y = math.floor((1 - py) * (h / 2))
+  self.point.screen.w = math.floor(pw       * (w / 2))
 end
 
-function Segment:addDeco()
+function Segment:altitude()
+  return self.point.world.y
 end
 
 function Segment:setColors(road, grass, rumble, lane)
